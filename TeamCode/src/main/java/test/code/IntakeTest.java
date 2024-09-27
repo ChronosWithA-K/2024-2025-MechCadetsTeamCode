aa652ba5fc990f880f2240996309d0d00f8c58f5
milo.lee27 <milolee1000@gmail.com>
2024-09-27 16:58:33 -0700

    add IntakeTest.java

---
+++ TeamCode/src/main/java/test/code/IntakeTest.java
@@ -0,0 +1,15 @@
+package test.codegrad;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "Basic: Intake Test")
+public class IntakeTest extends LinearOpMode{
+    @Override
+    public void runOpMode(){
+
+    }
+}
