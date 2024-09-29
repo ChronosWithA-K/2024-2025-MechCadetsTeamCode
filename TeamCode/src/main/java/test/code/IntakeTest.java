d0f09353580e73e82f0f3fc44290f540b4a74fbe
milo.lee27 <milolee1000@gmail.com>
2024-09-29 13:55:11 -0700

    add IntakeTest.java

--- TeamCode/src/main/java/test/code/IntakeTest.java
+++ TeamCode/src/main/java/test/code/IntakeTest.java
@@ -1,4 +1,4 @@
-package test.codegrad;
+package test.code;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -6,10 +6,60 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name = "Basic: Intake Test")
+@TeleOp(name = "Basic: Intake Test", group = "Test")
 public class IntakeTest extends LinearOpMode{
-    @Override
-    public void runOpMode(){
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor intakeMotor = null;
 
+    @Override
+    public void runOpMode() {
+        intakeMotor = hardwareMap.get(DcMotor.class, "intake_motor");
+
+        intakeMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        // Wait for the game to start (driver presses PLAY)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            final int LOW_SPEED = 33;
+            final int MEDIUM_SPEED = 66;
+            final int HIGH_SPEED = 100;
+
+            int motorPower = MEDIUM_SPEED;
+
+            if (gamepad1.a) {
+                motorPower = LOW_SPEED;
+            } else if (gamepad1.b) {
+                motorPower = MEDIUM_SPEED;
+            } else if (gamepad1.y) {
+                motorPower = HIGH_SPEED;
+            }
+
+            if (gamepad1.right_bumper) {
+                motorPower += 10;
+            } else if (gamepad1.left_bumper) {
+                motorPower -= 10;
+            }
+
+            if (motorPower < 0) {
+                motorPower = 0;
+            } else if (motorPower > 100) {
+                motorPower = 100;
+            }
+
+            if (gamepad1.dpad_up) {
+                intakeMotor.setPower(motorPower);
+            } else if (gamepad1.dpad_down) {
+                intakeMotor.setPower(-motorPower);
+            }
+
+            telemetry.addData("Wheel power", "%4.2f, %4.2f", motorPower);
+            telemetry.update();
+        }
     }
 }
