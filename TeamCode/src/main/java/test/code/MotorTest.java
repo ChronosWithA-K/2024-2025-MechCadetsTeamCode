5315295847e34fe0e1e33ec301261d2f5522952a
milo.lee27 <milolee1000@gmail.com>
2024-09-30 19:36:56 -0700

    implement switching between multiple motors

--- TeamCode/src/main/java/test/code/MotorTest.java
+++ TeamCode/src/main/java/test/code/MotorTest.java
@@ -1,21 +1,32 @@
 package test.code;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name = "Basic: Intake Test", group = "Test")
-public class IntakeTest extends LinearOpMode{
+@TeleOp(name = "Basic: Motor Test", group = "Test")
+public class MotorTest extends LinearOpMode{
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor intakeMotor = null;
+    private DcMotor activeMotor = null;
+    // To test more motors, add them to the below array
+    private String[] motorNames = {"ladder_motor", "intake_motor", "left_front", "left_back"};
+    private int activeMotorIndex = 0;
+
+    private void setMotor(){
+        setMotor(0);
+    }
+
+    private void setMotor(int delta){
+        activeMotorIndex += delta;
+        activeMotorIndex = (activeMotorIndex + motorNames.length) % motorNames.length;
+        activeMotor = hardwareMap.get(DcMotor.class, motorNames[activeMotorIndex]);
+        activeMotor.setDirection(DcMotor.Direction.FORWARD);
+    }
 
     @Override
     public void runOpMode() {
-        intakeMotor = hardwareMap.get(DcMotor.class, "intake_motor");
-
-        intakeMotor.setDirection(DcMotor.Direction.FORWARD);
+        setMotor();
 
         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
@@ -39,26 +50,36 @@ public class IntakeTest extends LinearOpMode{
             } else if (gamepad1.y) {
                 motorPower = HIGH_SPEED;
             } else if (gamepad1.x) {
-                motorPower = 50; // I know it's a magic number, but it's worse to call it MEDIUM_SPEED or similar, isn't it?
-            } else if (gamepad1.right_bumper) {
+                motorPower = 50; // I know it's a magic number, but it's worse to call it HALF_SPEED or similar, isn't it?
+            }
+
+            if (gamepad1.right_bumper) {
                 motorPower += 10;
             } else if (gamepad1.left_bumper) {
                 motorPower -= 10;
-            } else if (motorPower < 0) {
+            }
+
+            if (motorPower < 0) {
                 motorPower = 0;
             } else if (motorPower > 100) {
                 motorPower = 100;
-            } else {
-                motorPower = 0;
+            }
+
+            if (gamepad1.dpad_left){
+                setMotor(-1);
+            } else if (gamepad1.dpad_right){
+                setMotor(1);
             }
 
             if (gamepad1.dpad_up) {
-                intakeMotor.setPower(motorPower);
+                activeMotor.setPower(motorPower);
             } else if (gamepad1.dpad_down) {
-                intakeMotor.setPower(-motorPower);
+                activeMotor.setPower(-motorPower);
+            } else {
+                activeMotor.setPower(0);
             }
 
-            telemetry.addData("Wheel power: ", motorPower);
+            telemetry.addData(motorNames[activeMotorIndex]+" power: ", motorPower);
             telemetry.update();
         }
     }
