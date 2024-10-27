8a459c8d14ef335ca9fa70603230b10e3171f354
milo.lee27 <milolee1000@gmail.com>
2024-10-27 14:22:29 -0700

    add basic auto follow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoEncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoEncoderTest.java
@@ -12,6 +12,12 @@ import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower;
+import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierLine;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Path;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathChain;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
 
 import java.util.List;
 
@@ -33,7 +39,7 @@ import java.util.List;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Robot: Auto Encoder Test", group="Test")
+@Autonomous(name = "Robot: Auto Encoder Test", group = "Test")
 public class RobotAutoEncoderTest extends LinearOpMode {
 
     /* Declare OpMode members. */
@@ -42,12 +48,16 @@ public class RobotAutoEncoderTest extends LinearOpMode {
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
 
-    private DcMotor xEncoder = null;
-    private DcMotor yEncoder = null;
+//    private DcMotor xEncoder = null;
+//    private DcMotor yEncoder = null;
 
     private IMU imu = null;
 
     private Limelight3A limelight;
+
+    private Follower follower;
+
+    private Pose startPose = new Pose(0, 0, 0);
 
     private ElapsedTime runtime = new ElapsedTime();
 
@@ -57,9 +67,9 @@ public class RobotAutoEncoderTest extends LinearOpMode {
     // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
     // This is gearing DOWN for less speed and more torque.
     // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
-    static final double COUNTS_PER_MOTOR_REV  = 435 ;
-    static final double DRIVE_GEAR_REDUCTION = 1.0 ;     // No External Gearing.
-    static final double WHEEL_DIAMETER_INCHES = 4.09449 ; // For figuring circumference
+    static final double COUNTS_PER_MOTOR_REV = 435;
+    static final double DRIVE_GEAR_REDUCTION = 1.0;     // No External Gearing.
+    static final double WHEEL_DIAMETER_INCHES = 4.09449; // For figuring circumference
     static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
     static final double DRIVE_SPEED = 0.6;
     static final double TURN_SPEED = 0.5;
@@ -67,14 +77,17 @@ public class RobotAutoEncoderTest extends LinearOpMode {
     @Override
     public void runOpMode() {
 
+        follower = new Follower(hardwareMap);
+        follower.setStartingPose(startPose);
+
         // Initialize the drive system variables.
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front");
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front");
         leftBackDrive = hardwareMap.get(DcMotor.class, "left_back");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front");
         rightBackDrive = hardwareMap.get(DcMotor.class, "right_back");
 
-        xEncoder = hardwareMap.get(DcMotor.class, "x_encoder");
-        yEncoder = hardwareMap.get(DcMotor.class, "y_encoder");
+//        xEncoder = hardwareMap.get(DcMotor.class, "x_encoder");
+//        yEncoder = hardwareMap.get(DcMotor.class, "y_encoder");
 
         imu = hardwareMap.get(IMU.class, "imu");
 
@@ -115,78 +128,76 @@ public class RobotAutoEncoderTest extends LinearOpMode {
         limelight.pipelineSwitch(9);
 
         // Send telemetry message to indicate successful Encoder reset
-        telemetry.addData("Starting at",  "%7d :%7d",
-                leftFrontDrive.getCurrentPosition(),
-                leftBackDrive.getCurrentPosition(),
-                rightFrontDrive.getCurrentPosition(),
-                rightBackDrive.getCurrentPosition());
+        telemetry.addData("Starting at", "%7d :%7d", leftFrontDrive.getCurrentPosition(), leftBackDrive.getCurrentPosition(), rightFrontDrive.getCurrentPosition(), rightBackDrive.getCurrentPosition());
         telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
         LLStatus limelightStatus = limelight.getStatus();
-        telemetry.addData("Pipeline", "Index: %d, Type: %s",
-                limelightStatus.getPipelineIndex(), limelightStatus.getPipelineType());
+        telemetry.addData("Pipeline", "Index: %d, Type: %s", limelightStatus.getPipelineIndex(), limelightStatus.getPipelineType());
 
-        while(opModeIsActive()) {
-            LLResult limelightResult = limelight.getLatestResult();
+        LLResult limelightResult = limelight.getLatestResult();
 
-            if (limelightResult != null) {
-                limelight.updateRobotOrientation(imu.getRobotYawPitchRollAngles().getYaw());
-                // Access general information
-                Pose3D botpose = limelightResult.getBotpose();
+        if (limelightResult != null) {
+            limelight.updateRobotOrientation(imu.getRobotYawPitchRollAngles().getYaw());
+            // Access general information
+            Pose3D botpose = limelightResult.getBotpose();
 
-                double captureLatency = limelightResult.getCaptureLatency();
-                double targetingLatency = limelightResult.getTargetingLatency();
-                double parseLatency = limelightResult.getParseLatency();
-                telemetry.addData("LL Latency", captureLatency + targetingLatency);
-                telemetry.addData("Parse Latency", parseLatency);
-                telemetry.addData("PythonOutput", java.util.Arrays.toString(limelightResult.getPythonOutput()));
+            double captureLatency = limelightResult.getCaptureLatency();
+            double targetingLatency = limelightResult.getTargetingLatency();
+            double parseLatency = limelightResult.getParseLatency();
+            telemetry.addData("LL Latency", captureLatency + targetingLatency);
+            telemetry.addData("Parse Latency", parseLatency);
+            telemetry.addData("PythonOutput", java.util.Arrays.toString(limelightResult.getPythonOutput()));
 
-                if (limelightResult.isValid()) {
-                    double targetX = limelightResult.getTx(); // How far left or right the target is (degrees)
-                    double targetY = limelightResult.getTy(); // How far up or down the target is (degrees)
-                    double targetArea = limelightResult.getTa(); // How big the target looks (0%-100% of the image)
+            if (limelightResult.isValid()) {
+                double targetX = limelightResult.getTx(); // How far left or right the target is (degrees)
+                double targetY = limelightResult.getTy(); // How far up or down the target is (degrees)
+                double targetArea = limelightResult.getTa(); // How big the target looks (0%-100% of the image)
 
-                    telemetry.addData("Target X", targetX);
-                    telemetry.addData("Target Y", targetY);
-                    telemetry.addData("Target Area", targetArea);
+                telemetry.addData("Target X", targetX);
+                telemetry.addData("Target Y", targetY);
+                telemetry.addData("Target Area", targetArea);
 
-                    if (botpose != null) {
-                        double robotX = botpose.getPosition().x;
-                        double robotY = botpose.getPosition().y;
-                        double robotRotation = botpose.getOrientation().getYaw();
-                        telemetry.addData("MT1 Location", "(" + robotX + ", " + robotY + ", " + robotRotation + ")");
-                    }
+                if (botpose != null) {
+                    double robotX = botpose.getPosition().x;
+                    double robotY = botpose.getPosition().y;
+                    double robotRotation = botpose.getOrientation().getYaw();
+                    telemetry.addData("MT1 Location", "(" + robotX + ", " + robotY + ", " + robotRotation + ")");
+                }
 
-                    telemetry.addData("Botpose", botpose.toString());
+                telemetry.addData("Botpose", botpose.toString());
 
-                    // Access fiducial results
-                    List<LLResultTypes.FiducialResult> fiducialResults = limelightResult.getFiducialResults();
-                    for (LLResultTypes.FiducialResult fr : fiducialResults) {
-                        telemetry.addData("Fiducial", "ID: %d, Family: %s, X: %.2f, Y: %.2f", fr.getFiducialId(), fr.getFamily(), fr.getTargetXDegrees(), fr.getTargetYDegrees());
-                    }
+                // Access fiducial results
+                List<LLResultTypes.FiducialResult> fiducialResults = limelightResult.getFiducialResults();
+                for (LLResultTypes.FiducialResult fr : fiducialResults) {
+                    telemetry.addData("Fiducial", "ID: %d, Family: %s, X: %.2f, Y: %.2f", fr.getFiducialId(), fr.getFamily(), fr.getTargetXDegrees(), fr.getTargetYDegrees());
+                }
 
-                    // Access color results
-                    List<LLResultTypes.ColorResult> colorResults = limelightResult.getColorResults();
-                    for (LLResultTypes.ColorResult cr : colorResults) {
-                        telemetry.addData("Color", "X: %.2f, Y: %.2f", cr.getTargetXDegrees(), cr.getTargetYDegrees());
-                    }
-                } else {
-                    telemetry.addData("Limelight", "No targets");
+                // Access color results
+                List<LLResultTypes.ColorResult> colorResults = limelightResult.getColorResults();
+                for (LLResultTypes.ColorResult cr : colorResults) {
+                    telemetry.addData("Color", "X: %.2f, Y: %.2f", cr.getTargetXDegrees(), cr.getTargetYDegrees());
                 }
             } else {
-                telemetry.addData("Limelight", "No data available");
+                telemetry.addData("Limelight", "No targets");
             }
-            telemetry.update();
+        } else {
+            telemetry.addData("Limelight", "No data available");
+        }
+        telemetry.update();
+        while (opModeIsActive()) {
+            follower.followPath(follower.pathBuilder().addPath(new Path(new BezierLine(new Point(startPose), new Point(-36, 0, Point.CARTESIAN)))).build());
+            follower.update();
         }
 
         // Step through each leg of the path
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
-        encoderDrive(DRIVE_SPEED,  10,  10, 10, 10, 10); // S1: Forward 10 Inches with 10 Sec timeout
-        encoderDrive(TURN_SPEED,   12, 12, -12, -12, 10); // S2: Turn Right 12 Inches with 10 Sec timeout
-        encoderDrive(DRIVE_SPEED, -10, -10, -10, -10, 10); // S3: Reverse 10 Inches with 10 Sec timeout
+//        encoderDrive(DRIVE_SPEED,  10,  10, 10, 10, 10); // S1: Forward 10 Inches with 10 Sec timeout
+//        encoderDrive(TURN_SPEED,   12, 12, -12, -12, 10); // S2: Turn Right 12 Inches with 10 Sec timeout
+//        encoderDrive(DRIVE_SPEED, -10, -10, -10, -10, 10); // S3: Reverse 10 Inches with 10 Sec timeout
+
 
         telemetry.addData("Path", "Complete");
         telemetry.update();
@@ -201,10 +212,7 @@ public class RobotAutoEncoderTest extends LinearOpMode {
      *  2) Move runs out of time - timeoutS param is the time the robot has to drive a leg
      *  3) Driver stops the OpMode running.
      */
-    public void encoderDrive(double speed,
-                             double leftFrontInches, double leftBackInches,
-                             double rightFrontInches, double rightBackInches,
-                             double timeoutS) {
+    public void encoderDrive(double speed, double leftFrontInches, double leftBackInches, double rightFrontInches, double rightBackInches, double timeoutS) {
         int newLeftFrontTarget;
         int newLeftBackTarget;
         int newRightFrontTarget;
@@ -214,10 +222,10 @@ public class RobotAutoEncoderTest extends LinearOpMode {
         if (opModeIsActive()) {
 
             // Determine new target position, and pass to motor controller
-            newLeftFrontTarget = leftFrontDrive.getCurrentPosition() + (int)(leftFrontInches * COUNTS_PER_INCH);
-            newLeftBackTarget = leftBackDrive.getCurrentPosition() + (int)(leftBackInches * COUNTS_PER_INCH);
-            newRightFrontTarget = rightFrontDrive.getCurrentPosition() + (int)(rightFrontInches * COUNTS_PER_INCH);
-            newRightBackTarget = rightBackDrive.getCurrentPosition() + (int)(rightBackInches * COUNTS_PER_INCH);
+            newLeftFrontTarget = leftFrontDrive.getCurrentPosition() + (int) (leftFrontInches * COUNTS_PER_INCH);
+            newLeftBackTarget = leftBackDrive.getCurrentPosition() + (int) (leftBackInches * COUNTS_PER_INCH);
+            newRightFrontTarget = rightFrontDrive.getCurrentPosition() + (int) (rightFrontInches * COUNTS_PER_INCH);
+            newRightBackTarget = rightBackDrive.getCurrentPosition() + (int) (rightBackInches * COUNTS_PER_INCH);
             leftFrontDrive.setTargetPosition(newLeftFrontTarget);
             leftBackDrive.setTargetPosition(newLeftBackTarget);
             rightFrontDrive.setTargetPosition(newRightFrontTarget);
@@ -242,17 +250,11 @@ public class RobotAutoEncoderTest extends LinearOpMode {
             // always end the motion as soon as possible.
             // However, if you require that BOTH motors have finished their moves before the robot continues
             // onto the next step, use (isBusy() || isBusy()) in the loop test.
-            while (opModeIsActive() &&
-                    (runtime.seconds() < timeoutS) &&
-                    (leftFrontDrive.isBusy() && leftBackDrive.isBusy() &&
-                     rightFrontDrive.isBusy() && rightBackDrive.isBusy())) {
+            while (opModeIsActive() && (runtime.seconds() < timeoutS) && (leftFrontDrive.isBusy() && leftBackDrive.isBusy() && rightFrontDrive.isBusy() && rightBackDrive.isBusy())) {
 
                 // Display it for the driver.
-                telemetry.addData("Running to",  " %7d :%7d", newLeftFrontTarget, newLeftBackTarget,
-                        newRightFrontTarget, newRightBackTarget);
-                telemetry.addData("Currently at",  " at %7d :%7d",
-                        leftFrontDrive.getCurrentPosition(), leftBackDrive.getCurrentPosition(),
-                        rightFrontDrive.getCurrentPosition(), rightBackDrive.getCurrentPosition());
+                telemetry.addData("Running to", " %7d :%7d", newLeftFrontTarget, newLeftBackTarget, newRightFrontTarget, newRightBackTarget);
+                telemetry.addData("Currently at", " at %7d :%7d", leftFrontDrive.getCurrentPosition(), leftBackDrive.getCurrentPosition(), rightFrontDrive.getCurrentPosition(), rightBackDrive.getCurrentPosition());
                 telemetry.update();
             }
 
