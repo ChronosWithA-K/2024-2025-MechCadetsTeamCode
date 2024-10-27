0f25a20399c14683c000eb57aa9facf0662dd04d
milo.lee27 <milolee1000@gmail.com>
2024-10-27 15:08:08 -0700

    change scaleRange()

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomHolonomicDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomHolonomicDrive.java
@@ -44,7 +44,7 @@ public class CustomHolonomicDrive extends LinearOpMode {
         intakeServo = hardwareMap.get(Servo.class, "intake_servo");
         clawServo = hardwareMap.get(Servo.class, "claw_servo");
         clawWristServo = hardwareMap.get(Servo.class,  "claw_wrist_servo");
-        clawWristServo.scaleRange(0.5, 1.0);
+        clawWristServo.scaleRange(0.0, 0.5);
 
         viperSlideMotor = hardwareMap.get(DcMotor.class, "viper_slide_motor");
 
