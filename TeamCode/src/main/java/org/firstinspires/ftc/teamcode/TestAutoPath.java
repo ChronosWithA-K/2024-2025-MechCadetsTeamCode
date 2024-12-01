bd6b2964ff76e124d513f5d661214a0d30890710
Ayansh.Singh <ayansh.singh27@student.tamdistrict.org>
2024-12-01 15:58:28 -0800

    added more updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutoPath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutoPath.java
@@ -17,14 +17,19 @@ public class TestAutoPath extends LinearOpMode {// Declare OpMode members.
     static final double DRIVE_GEAR_REDUCTION = 1.0;     // No External Gearing.
     static final double WHEEL_DIAMETER_INCHES = 4.09449; // For figuring circumference
     static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double DRIVE_SPEED = 0.6;
+    static final double DRIVE_SPEED = 0.4;
     static final double TURN_SPEED = 0.5;
     private DcMotor viperSlideMotor = null;
-    int liftTopBar = 2890;
+    private Servo specimenClawServo = null;
+    private Servo scs = null;
 
+    int liftTopBar = 2890;
+    int engage_claw = 2790;
 
     @Override
     public void runOpMode() {
+        double specimenClawClosed = 0;
+        double specimenClawOpen = 0.5;
         // Initialize the hardware variables. Note that the strings used here must correspond
         // to the names assigned during the robot configuration step on the DS or RC devices.
         leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front");
@@ -47,26 +52,41 @@ public class TestAutoPath extends LinearOpMode {// Declare OpMode members.
         viperSlideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         viperSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         viperSlideMotor.setPower(1);
-        viperSlideMotor.setDirection(DcMotor.Direction.FORWARD);
+        viperSlideMotor.setDirection(DcMotor.Direction.REVERSE);
 
         leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        specimenClawServo = hardwareMap.get(Servo.class, "specimen_claw_servo");
+        scs = hardwareMap.get(Servo.class, "intake_servo");
+
+
         // Wait for the game to start (driver presses START)
         waitForStart();
         runtime.reset();
-
-        // run until the end of the match (driver presses STOP)
-
-        encoderDrive(DRIVE_SPEED, 24, 24, 24, 24, 10); // forward
-
-        viperSlideMotor.setTargetPosition(liftTopBar);
+        scs.setPosition(0);
+        //encoderDrive(DRIVE_SPEED, 22, 22, 22, 22, 10); // Forward 1 square
 
         while (opModeIsActive()) {
-
-
+            if (runtime.seconds() < 2){
+                specimenClawServo.setPosition(specimenClawClosed);
+                viperSlideMotor.setTargetPosition(liftTopBar);
+            }
+            else if (runtime.seconds() < 5){
+                specimenClawServo.setPosition(specimenClawClosed);
+            }
+//            viperSlideMotor.setTargetPosition(engage_claw);
+            if (viperSlideMotor.isBusy()) {
+                telemetry.addLine("Viper Enabled");
+                viperSlideMotor.setPower(1);
+            } else if (!viperSlideMotor.isBusy() && viperSlideMotor.getCurrentPosition() == 0) {
+                telemetry.addLine("Viper Disabled");
+                viperSlideMotor.setPower(0);
+            }
+            telemetry.addData("Viper Target: ", liftTopBar);
+            telemetry.addData("Viper Position: ", viperSlideMotor.getCurrentPosition());
             telemetry.update();
         }
     }
@@ -89,9 +109,6 @@ public class TestAutoPath extends LinearOpMode {// Declare OpMode members.
             leftBackDrive.setTargetPosition(newLeftBackTarget);
             rightFrontDrive.setTargetPosition(newRightFrontTarget);
             rightBackDrive.setTargetPosition(newRightBackTarget);
-
-
-
 
             // Turn on RUN_TO_POSITION
             leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -116,6 +133,8 @@ public class TestAutoPath extends LinearOpMode {// Declare OpMode members.
                 telemetry.addData("Running to", " %7d :%7d", newLeftFrontTarget, newLeftBackTarget, newRightFrontTarget, newRightBackTarget);
                 telemetry.addData("Currently at", " at %7d :%7d", leftFrontDrive.getCurrentPosition(), leftBackDrive.getCurrentPosition(), rightFrontDrive.getCurrentPosition(), rightBackDrive.getCurrentPosition());
                 telemetry.update();
+
+
             }
 
             // Stop all motion;
